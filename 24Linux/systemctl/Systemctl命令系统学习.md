e42e86b775de7bfa6db3f175f99033a557da87ff
tramplpf <1428164175@qq.com>
2022-04-09 21:56:19 +0800

    ok

---
+++ 24Linux/systemctl/Systemctl命令系统学习.md
@@ -0,0 +1,6 @@
+# Systemctl命令系统学习
+
+
+
+Systemctl 接受服务（.service）, 挂载点（.mount）, 套接字（.socket）和设备（.device）作为单元。 
+
---
+++ RabbitMQ/demo/src/test/java/com/lpf/study/rabbitmqdemo/RabbitmqdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpf.study.rabbitmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
