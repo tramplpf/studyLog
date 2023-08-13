6ce726900799675dcfa80d58339b7dcf34e30529
lipf <1428164175@qq.com>
2023-08-13 10:14:44 +0800

    delete

---
+++ Springboot学习记录/springboot3/04_gradle_build/gradleDemo3/src/main/java/com/study/gradleDemo3/GradleDemo3Application.java
@@ -0,0 +1,13 @@
+package com.study.gradleDemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDemo3Application.class, args);
+	}
+
+}
--- Springboot学习记录/springboot3/04_gradle_build/gradleDemo3/src/test/java/com/study/gradleDemo3/GradleDemo3ApplicationTests.java
+++ Springboot学习记录/springboot3/04_gradle_build/gradleDemo3/src/test/java/com/study/gradleDemo3/GradleDemo3ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.study.gradleDemo;
+package com.study.gradleDemo3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GradleDemoApplicationTests {
+class GradleDemo3ApplicationTests {
 
 	@Test
 	void contextLoads() {
