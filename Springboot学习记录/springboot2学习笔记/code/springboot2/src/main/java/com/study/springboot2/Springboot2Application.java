a3eb8911fde1696aad98bdd701055dc5e2aeb4cc
lipf <1428164175@qq.com>
2023-09-07 20:28:24 +0800

    dbeaver的使用

---
+++ Springboot学习记录/springboot2学习笔记/code/springboot2/src/main/java/com/study/springboot2/Springboot2Application.java
@@ -0,0 +1,19 @@
+package com.study.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+*
+*  启动类
+* */
+@SpringBootApplication
+public class Springboot2Application {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2Application.class, args);
+	}
+
+}
