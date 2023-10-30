9ac33e8668ba21d2280c9b186f8a69ed491ef47b
lipf <1428164175@qq.com>
2023-10-30 09:56:28 +0800

    dbeaver工具结果集显示面板的布局调整

--- Springboot学习记录/springboot2学习笔记/code/springboot2/src/main/java/com/study/springboot2/Springboot2Application.java
+++ Springboot学习记录/springboot2学习笔记/code/springboot2/src/main/java/com/study/springboot2/Springboot2Application.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Springboot2Application {
 
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2Application.class, args);
 	}
